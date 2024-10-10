997e732669b9a642b0b3be98d32129f4e9460e28
qingyuhms <qingyu.hms@gmail.com>
2024-10-10 18:39:02 -0400

    fixed index.html

--- render.R
+++ render.R
@@ -1,13 +1,18 @@
 #run this script to knit all .rmd files
 # List of Rmd files
 rmd_files <- list.files(path = ".", pattern = "\\.Rmd$", full.names = TRUE)
-normalizePath(rmd_files[1])
 
 # Render each file
 
 for (file in rmd_files) {
-  path=dirname(normalizePath(file))
-  outDir=paste0(path,'/docs')
-  print(outDir)
-  rmarkdown::render(file,params = list(output_dir = outDir),output_options = list(lib_dir=outDir))
+
+  Newfile=rmarkdown::render(file,output_options = list(lib_dir='./docs/libs'))
+
+  file.copy(Newfile,'./docs/')
+  file.remove(Newfile)
 }
+
+
+Newfile=rmarkdown::render(rmd_files[1],output_options = list(lib_dir='./docs/libs'))
+file.copy(Newfile,'./docs/')
+file.remove(Newfile)
